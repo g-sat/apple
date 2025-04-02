3eb843ec97cda265f0e3c8eef25e649cd0a01f6c
g-sat <g.satl0107@gmail.com>
2025-04-02 22:31:59 +0530

    third commit for build

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}, // Autoprefixer is configured here, not in Vite plugins
+    },
+  }
+  
---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+export default {
+    plugins: {
+      // Your PostCSS plugins here
+      autoprefixer: {},
+      // other plugins...
+    },
+  };
